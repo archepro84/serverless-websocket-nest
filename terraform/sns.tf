ed0f8da5128b928111c13d0cb048f7ffb6b1c8c0
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-04-24 16:44:25 +0900

    feat: Remove AWS SNS

--- terraform/sns.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_sns_topic" "chats_messages" {
-  name = local.chat_topic_name
-
-  tags = {
-    name = "${local.project_name}-${local.chat_topic_name}"
-  }
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,6 @@ locals {
   env                   = data.dotenv.dev_config.env
   project_name          = try(local.env.PROJECT_NAME, "serverless-websocket-nest")
   region                = try(local.env.REGION, "ap-northeast-2")
-  chat_topic_name       = try(local.env.CHAT_TOPIC_NAME, "chat_topic_name")
   users_table_name      = try(local.env.USERS_TABLE, "users_table_name")
   chat_rooms_table_name = try(local.env.CHAT_ROOMS_TABLE, "chat_rooms_table_name")
   chats_table_name      = try(local.env.CHATS_TABLE, "chats_table_name")
